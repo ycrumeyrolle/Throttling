bb4c31452a66ab576b7abcbe908b90e9e0db7a80
Yann Crumeyrolle <ycrumeyrolle@free.fr>
2015-05-23 20:49:45 +0200

    Add API key providers

---
+++ src/Throttling.Core/KeyProviders/FormClientKeyProvider.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Http;
+using Microsoft.Framework.Internal;
+
+namespace Throttling
+{
+    public class FormClientKeyProvider : IClientKeyProvider
+    {
+        private readonly string _parameter;
+
+        public FormClientKeyProvider([NotNull] string parameter)
+        {
+            _parameter = parameter;
+        }
+
+        public string GetKey([NotNull] HttpContext context)
+        {
+            var apiKeys = context.Request.Form.GetValues(_parameter);
+
+            if (apiKeys != null && apiKeys.Count > 0)
+            {
+                return apiKeys[0];
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/Throttling.Core/KeyProviders/QueryStringKeyProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNet.Http;
+using Microsoft.Framework.Internal;
+
+namespace Throttling
+{
+    public class QueryStringClientKeyProvider : IClientKeyProvider
+    {
+        private readonly string _parameter;
+
+        public QueryStringClientKeyProvider([NotNull] string parameter) 
+        {
+            _parameter = parameter;
+        }
+
+        public string GetKey([NotNull] HttpContext context)
+        {
+            var keys = context.Request.Query.GetValues(_parameter);
+            if (keys != null && keys.Count > 0)
+            {
+                return keys[0];
+            }
+
+            return null;
+        }
+    }
+}
